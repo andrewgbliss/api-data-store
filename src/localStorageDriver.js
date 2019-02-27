057c432c3c103a3734ee025dbeb7e1dbc93ceb7a
Andrew Bliss <andrewgbliss@gmail.com>
2019-02-27 09:37:33 -0700

    updates

--- src/localStorageDriver.js
+++ src/localStorageDriver.js
@@ -1,19 +1,16 @@
 const moment = require('moment');
 const attempt = require('lodash/attempt');
-const pull = require('lodash/pull');
 const filter = require('lodash/filter');
 
 const PREFIX = 'api-data-store:';
 
 class LocalStorageDriver {
-  constructor() {
-    this.keys = filter(Object.keys(localStorage), key => {
-      return key.indexOf(PREFIX) === 0;
-    });
-  }
 
   clear() {
-    this.keys.forEach(key => {
+    const keys = filter(Object.keys(localStorage), key => {
+      return key.indexOf(PREFIX) === 0;
+    });
+    keys.forEach(key => {
       localStorage.removeItem(PREFIX + key);
     });
   }
@@ -26,12 +23,10 @@ class LocalStorageDriver {
   setItem(key, value) {
     const item = { value, timestamp: moment().unix() };
     localStorage.setItem(PREFIX + key, JSON.stringify(item));
-    this.keys.push(key);
   }
 
   removeItem(key) {
     localStorage.removeItem(PREFIX + key);
-    this.keys = pull(this.keys, key);
   }
 
   reset() {
@@ -43,7 +38,9 @@ class LocalStorageDriver {
   }
 
   getKeys() {
-    return this.keys;
+    return filter(Object.keys(localStorage), key => {
+      return key.indexOf(PREFIX) === 0;
+    });
   }
 }
 
